52607944138abafdba0f3495799744d7eb496950
wendellvalois <wendellvalois@172.16.32.97>
2016-05-10 00:24:11 -0300

    um pouco mais de rudimentos de banco

--- src/java/br/com/cantinasjuquinha/dao/JDBCResponsavelDAO.java
+++ src/java/br/com/cantinasjuquinha/dao/JDBCResponsavelDAO.java
@@ -9,35 +9,36 @@ import br.com.cantinasjuquinha.bean.Responsavel;
 import br.com.cantinasjuquinha.util.ConnectionFactory;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
 
 /**
  *
  * @author wendellvalois
  */
-public class JDBCResponsavelDAO implements ResponsavelDAO{
-    
-    
+public class JDBCResponsavelDAO implements ResponsavelDAO {
+
     Connection connection;
-    public JDBCResponsavelDAO(){
-        
+
+    public JDBCResponsavelDAO() {
+
         connection = ConnectionFactory.getConnection();
     }
 
     @Override
     public void inserir(Responsavel responsavel) {
         try {
- /*           String SQL = "insert into (nome, cpf, telefone, email, login, senha) values"
+            /*           String SQL = "insert into (nome, cpf, telefone, email, login, senha) values"
                     + "(?,?,?,?,?,?)";
-*/
-             String SQL = "insert into responsavel (cpfresponsavel, nomeresponsavel, telefoneresponsavel, emailresponsavel, enderecoresponsavel) values"
+             */
+            String SQL = "insert into responsavel (cpfresponsavel, nomeresponsavel, telefoneresponsavel, emailresponsavel, enderecoresponsavel) values"
                     + "(?,?,?,?,?)";
             PreparedStatement ps = connection.prepareStatement(SQL);
-            
+
             ps.setString(1, responsavel.getCpf());
             ps.setString(2, responsavel.getNome());
             ps.setString(3, responsavel.getTelefone());
@@ -45,33 +46,92 @@ public class JDBCResponsavelDAO implements ResponsavelDAO{
             ps.setString(5, responsavel.getEndereco());
             //ps.setString(5, responsavel.getLogin());
             //ps.setString(6, responsavel.getSenha());
-            
+
             ps.executeUpdate();
         } catch (SQLException ex) {
             Logger.getLogger(JDBCResponsavelDAO.class.getName()).log(Level.SEVERE, null, ex);
+            throw new RuntimeException("Erro ao inserir Responsavel", ex);
         }
-        
+
     }
 
     @Override
     public void remover(int id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try {
+            String SQL = "delete from responsavel where id= ?";
+            PreparedStatement ps = connection.prepareStatement(SQL);
+            ps.setInt(1, id);
+            ps.executeUpdate();
+
+            ps.close();
+
+        } catch (SQLException ex) {
+            Logger.getLogger(JDBCResponsavelDAO.class.getName()).log(Level.SEVERE, null, ex);
+            throw new RuntimeException("Erro ao remover registro de Responsavel", ex);
+        }
     }
 
     @Override
     public List<Responsavel> listar() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        List<Responsavel> responsaveis = new ArrayList<Responsavel>();
+        try {
+            String SQL = "select * from responsavel";
+            PreparedStatement ps = connection.prepareStatement(SQL);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                Responsavel responsavel = new Responsavel();
+                responsavel.setNome(rs.getString("nome"));
+                responsavel.setEndereco(rs.getString("bairro"));
+                //responsavel.getEndereco()setRua(rs.getString("rua")); // com Endereco composto de vario valores
+                responsaveis.add(responsavel);
+
+            }
+            ps.close();
+            rs.close();
+            return responsaveis;
+
+        } catch (SQLException ex) {
+            Logger.getLogger(JDBCResponsavelDAO.class.getName()).log(Level.SEVERE, null, ex);
+            throw new RuntimeException("Falha ao listar Responsaveis em JDBC");
+        }
     }
 
     @Override
     public Responsavel buscar(int id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try {
+            Responsavel responsavel = new Responsavel();
+            String SQL = "select * from responsavel where id = ?";
+            PreparedStatement ps = connection.prepareStatement(SQL);
+            ResultSet rs = ps.executeQuery();
+
+            rs.next();
+            responsavel.setNome("nome");
+            responsavel.setEndereco("endereco");
+            responsavel.setEndereco("endereco");
+            responsavel.setEmail("email");
+
+            return responsavel;
+
+        } catch (SQLException ex) {
+            Logger.getLogger(JDBCResponsavelDAO.class.getName()).log(Level.SEVERE, null, ex);
+            throw new RuntimeException("Erro ao Buscar Registro de Responsaveis");
+        }
     }
 
     @Override
     public void editar(Responsavel responsavel) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try {
+            String SQL = "update pessoa set nome=? endereco= ? where cpf =?;";
+            PreparedStatement ps = connection.prepareStatement(SQL);
+            ps.setString(1, responsavel.getNome());
+            ps.setString(2, responsavel.getEndereco());
+            ps.setString(3, responsavel.getCpf());
+            ps.executeUpdate();
+            ps.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(JDBCResponsavelDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
     }
-    
-    
+
 }
